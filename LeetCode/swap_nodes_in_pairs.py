b769a860e35d99949c5ce13f64aeabfcb0f49582
shouliang <shouliang0816@163.com>
2018-10-28 23:17:09 +0800

    add LeetCode

---
+++ LeetCode/swap_nodes_in_pairs.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        # pre->a->b->c->…链表，交换a、a节点即把a.next指向c(c=b.next)，b.next指向a，p.next指向b
+        pre = self
+        pre.next = head
+        while pre.next and pre.next.next:
+            a = pre.next
+            b = pre.next.next
+
+            # 变换指针，a,b互相交换： a.next = b.next,b.next = a ，顺序可不颠倒且是连续的两句
+            # 否则会失去b.next,从而a.next指向不了b.next
+            # 但是pre.next = b可在前或在后
+            a.next = b.next
+            b.next = a
+            pre.next = b
+
+            # pre指向下一个指针
+            pre = a
+        return self.next
---
+++ Python/enumerate.py
@@ -0,0 +1,5 @@
+seasons = ['Spring', 'Summer', 'Fall', 'Winter']
+
+# enumerate()将一个可遍历的数据对象组合为一个索引序列，同时列出下标和数据
+for i, season in enumerate(seasons):
+    print(i, season)
