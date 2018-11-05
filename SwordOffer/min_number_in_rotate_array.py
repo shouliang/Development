95ca75d3d31022f02621ea07f22dc5f304b2914c
shouliang <shouliang0816@163.com>
2018-11-05 23:00:30 +0800

    add min rotate and sliding window maximum

--- SwordOffer/min_number_in_rotate_array.py
+++ SwordOffer/min_number_in_rotate_array.py
@@ -1,33 +1,46 @@
 # coding=utf-8
 
 # 旋转数组的最小数字： 如：1，2，3, 4, 5 旋转为3，4，5，1，2
+# 局部有序，利用二分查找的思想来相似地解决
 class Solution:
-    def findMin(self, nums):
-        if not nums:
+    def minNumberInRotateArray(self, rotateArray):
+        if not rotateArray:
             return 0
 
         start = 0
-        end = len(nums) - 1
+        end = len(rotateArray) - 1
         middle = start
-        while nums[start] >= nums[end]:
+        while rotateArray[start] >= rotateArray[end]:
             if end - start == 1:
                 middle = end
                 break
 
             middle = start + ((end - start) >> 1)
 
-            if nums[middle] >= nums[start]:
+            # 相等则顺序查找
+            if rotateArray[start] == rotateArray[middle] and rotateArray[end] == rotateArray[middle]:
+                self.minInorder(rotateArray, start, end)
+
+            if rotateArray[middle] >= rotateArray[start]:
                 start = middle
-            elif nums[middle] <= nums[start]:
+            elif rotateArray[middle] <= rotateArray[start]:
                 end = middle
 
-        return nums[middle]
+        return rotateArray[middle]
+
+    # 顺序查找最小值
+    def minInorder(self, rotateArray, start, end):
+        minNum = rotateArray[start]
+        length = end - start
+        for i in range(length):
+            if rotateArray[start + i] < minNum:
+                minNum = rotateArray[start + i]
+        return minNum
 
 
 s = Solution()
 
-num = s.findMin([3, 4, 5, 1, 2])
+num = s.minNumberInRotateArray([3, 4, 5, 1, 2])
 print(num)
 
-# num = s.findMin([4, 5, 2, 3])
-# print(num)
+
