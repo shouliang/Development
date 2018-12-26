55f5ad2d3de65d4cc49d3dc252df1b8bd095e528
shouliang <shouliang0816@163.com>
2018-12-26 20:30:45 +0800

    add merge sort

--- PyDS/merge_sort.py
+++ PyDS/merge_sort.py
@@ -1,9 +1,10 @@
 def merge_sort(alist):
     merge_sort_helper(alist, 0, len(alist) - 1)
 
+
 def merge_sort_helper(alist, low, high):
     if low < high:
-        mid = low + (high - low) // 2
+        mid = (low + high) // 2
         merge_sort_helper(alist, low, mid)
         merge_sort_helper(alist, mid + 1, high)
         merge(alist, low, mid, high)
@@ -13,19 +14,26 @@ def merge(alist, low, mid, high):
     i, j = low, mid + 1
     tmp = []
 
-    while i <= mid and j <= high:
+    while i < mid and j < high:
         if alist[i] <= alist[j]:
             tmp.append(alist[i])
             i += 1
         else:
             tmp.append(alist[j])
             j += 1
-    # start = i if i <= mid else j
-    # end = mid if i <= mid else high
+
+    # start = i if i < mid else j
+    # end = mid if i < mid else high
     # tmp.extend(alist[start:end + 1])
     # alist[low:high + 1] = tmp
+    while i < mid:
+        print(i,mid)
+        tmp.append(alist[i])
+        i = i +1
 
 
-a1 = [3, 5, 6, 7, 8]
+
+
+a1 = [3, 5, 6, 7, 8, 1]
 merge_sort(a1)
 print(a1)
