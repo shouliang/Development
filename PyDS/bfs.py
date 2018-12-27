81a4cf25a77ab701307cf62cb3020339bc31f670
shouliang <shouliang0816@163.com>
2018-12-27 23:15:01 +0800

    add bfs dfs

--- PyDS/bfs.py
+++ PyDS/bfs.py
@@ -1,14 +1,15 @@
+# https://eddmann.com/posts/depth-first-search-and-breadth-first-search-in-python/
+# 广度搜索
 def bfs(graph, start):
-    visited = set()
-    queue = [start]
+    visited, queue = set(), [start]
     while queue:
         vertex = queue.pop(0)
         if vertex not in visited:
+            print(vertex)
             visited.add(vertex)
             queue.extend(graph[vertex] - visited)
 
     return visited
-
 
 
 graph = {'A': set(['B', 'C']),
@@ -18,5 +19,4 @@ graph = {'A': set(['B', 'C']),
          'E': set(['B', 'F']),
          'F': set(['C', 'E'])}
 
-visited = bfs(graph,'A')
-print(visited)
+visited = bfs(graph, 'A')
---
+++ PyDS/dfs.py
@@ -0,0 +1,30 @@
+# https://eddmann.com/posts/depth-first-search-and-breadth-first-search-in-python/
+# 深度搜索
+def dfs(graph,start):
+    visited, stack = set(),[start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            print(vertex)
+            visited.add(vertex)
+            stack.extend(graph[vertex] - visited)
+    return visited
+
+def def_recursive(graph,start,visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    for next in graph[start] - visited:
+        dfs(graph, next, visited)
+    return visited
+
+graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}
+
+visited = dfs(graph,'A')
+
+
