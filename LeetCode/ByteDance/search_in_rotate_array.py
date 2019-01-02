b47c2b2ee45abed3f2b1c2083340750a80a4ba33
shouliang <shouliang0816@163.com>
2019-01-03 00:05:58 +0800

    add search

--- LeetCode/ByteDance/search_in_rotate_array.py
+++ LeetCode/ByteDance/search_in_rotate_array.py
@@ -1,3 +1,9 @@
+'''
+leetCode33. 搜索旋转排序数组
+思路：二分查找，主要是确定单调递增的那一段区间
+'''
+
+
 class Solution:
     def search(self, nums, target):
         """
@@ -5,24 +11,24 @@ class Solution:
         :type target: int
         :rtype: int
         """
-        left = 0
-        right = len(nums) - 1
+        left, right = 0, len(nums) - 1
         while left <= right:
-            mid = left + (right - left) // 2
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
             # mid左端是排好序的
             if nums[mid] >= nums[left]:
-                # 因为前面第11行的判断已经说明nums[mid] != target了，所以不再考虑nums[mid] = target的情况
-                if nums[mid] > target and nums[left] <= target:
+                # 因为前面第11行的判断已经说明A[mid] != target了，所以不再考虑A[mid] = target的情况
+                if target >= nums[left] and target < nums[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
-            # 下面的else等同于elif nums[mid] <= nums[right]
+            # 下面的else等同于elif A[mid] <= A[right]
             else:
                 if nums[mid] < target and nums[right] >= target:
                     left = mid + 1
                 else:
                     right = mid - 1
-
         return -1
 
 
@@ -32,8 +38,8 @@ class Solution:
 nums = [4, 5, 6, 7, 0, 1, 2]
 target = 0
 
-# nums = [1]
-# target = 0
+nums = [3, 1]
+target = 1
 
 s = Solution()
 posotion = s.search(nums, target)
