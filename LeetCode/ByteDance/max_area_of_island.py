166d5d97b8e1003ceafe45480e0e840bcbde65b4
朱守亮 <git@git.dxl.cc:node/hunqing.git>
2019-01-09 18:37:28 +0800

    add max area of island

--- LeetCode/ByteDance/max_area_of_island.py
+++ LeetCode/ByteDance/max_area_of_island.py
@@ -20,6 +20,12 @@
 对于上面这个给定的矩阵, 返回 0。
 
 注意: 给定的矩阵grid 的长度和宽度都不超过 50。
+
+思路：
+DFS：Depth-first search，深度优先搜索。
+用DFS算法实现每个区域的面积，这里利用递归实现，计算过的陆地则变为0；
+遍历图中所有的元素，记录当前最大的面积。
+
 '''
 
 
@@ -32,33 +38,21 @@ class Solution:
         maxArea = 0
         rows = len(grid)
         cols = len(grid[0]) if rows else 0
-        visited = [[0] * cols] * rows
 
+        # 对二维数组中的每个结点进行DFS,然后求出最大值
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j]:
-                    maxArea = max(maxArea, dfs(
-                        grid, i, j, rows, cols, visited, maxArea))
+                    maxArea = max(maxArea, dfs(grid, i, j, rows, cols))
         return maxArea
 
 
-def dfs(grid, i, j, rows, cols, visited, result):
-    if not result:
-        result = 1
-    else:
-        result += 1
-    visited[i][j] = 1
-    if i > 0 and i < rows and j > 0 and j < cols:
-        if not visited[i-1][j] and grid[i-1][j] == 1:
-            dfs(grid, i-1, j, rows, cols, visited, result)
-        if not visited[i+1][j] and grid[i+1][j] == 1:
-            dfs(grid, i+1, j, rows, cols, visited, result)
-        if not visited[i][j-1] and grid[i][j-1] == 1:
-            dfs(grid, i, j-1, rows, cols, visited, result)
-        if not visited[i][j+1] and grid[i][j+1] == 1:
-            dfs(grid, i, j+1, rows, cols, visited, result)
-
-    return result
+def dfs(grid, i, j, rows, cols):
+    # i,j的边界界定，i和j均从0开始
+    if i >= 0 and i < rows and j >= 0 and j < cols and grid[i][j] != 0:
+        grid[i][j] = 0   # 访问过的则置为0，这样可以省略掉访问数组的形式
+        return 1 + dfs(grid, i-1, j, rows, cols) + dfs(grid, i+1, j, rows, cols) + dfs(grid, i, j-1, rows, cols) + dfs(grid, i, j+1, rows, cols)
+    return 0
 
 
 grid = [
