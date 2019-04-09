7c3a5d4080cebf8ff98cdd7822ec7f48e6b31b9b
shouliang <shouliang0816@163.com>
2019-04-09 20:41:44 +0800

    combine

--- AcmZju/input_two_sum.py
+++
@@ -1,9 +0,0 @@
-# 控制台输入两个数，以空格割开，求两数之和
-# http://acm.zju.edu.cn/onlinejudge/showProblem.do?problemId=1
-while True:
-    try:
-        line = input()
-        num = line.split()
-        print(int(num[0]) + int(num[1]))
-    except:
-        break
