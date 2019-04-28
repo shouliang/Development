c85b53f07cab446f866efdfa17b9a622231c99f3
朱守亮 <git@git.dxl.cc:node/hunqing.git>
2019-04-28 11:09:41 +0800

    modify sort

--- Python/PyDS/sort/insertion_sort.py
+++ Python/PyDS/sort/insertion_sort.py
@@ -1,10 +1,12 @@
 # coding=utf-8
+# 从未排序部分选择第1个位置，插入到已排序部分的合适位置，初始化时已排序的部分就是第0个元素，故未排序的从第1个位置开始
 def insertionSort(nums):
     n = len(nums)
     for i in range(1, n):     # i从1开始，因为是从未排序的向已排序的部分插入，开始已排序的位数组第一个元素
         itemToInsert = nums[i]
-        j = i - 1
 
+        # 从 i - 1 到 0 找一个位置插入，同时移动元素，插入的位置为 j + 1
+        j = i - 1
         # 循环直到找到比itemToInsert要小的元素，将itemToInsert插入到j的后面一位，即j+1
         while j >= 0 and nums[j] > itemToInsert:
             nums[j + 1] = nums[j]  # 移动元素
--- Python/PyDS/sort/selection_sort.py
+++ Python/PyDS/sort/selection_sort.py
@@ -1,19 +1,22 @@
 # n个数，第1次找出n个数中最小的放到第1个位置
 #       第2次  找出后面n-1个数中最小的放到第2个位置
 #       第n-1次找出后面2个数中最小的放到第n-1个位置
-#       第n次  找出后面1个数中最小的放到第n个位置,故第n次可以不用做，即外层循环为n-1次
+
+#       第n次  找出后面1个数中最小的放到第n个位置,已经是1个数了，故第n次可以不用做，即外层循环为n-1次
 
 def selectionSort(nums):
     n = len(nums)
-    for i in range(n-1):  # 外层循环只需要n-1次
-        minIndex = i
-        for j in range(i + 1, n):
+    for i in range(n-1):  # 外层循环只需要n-1次，从0到n-1
+        minIndex = i   
+        for j in range(i + 1, n):  # 从 i 到 n 中 中找出最小的,因为初始化时minIndex = i 也参与比较
             if nums[j] < nums[minIndex]:
                 minIndex = j
 
         # i如果已经在正确的位置上，则不需要交换
         if minIndex != i:
-            swap(lyst, minIndex, i)
+            swap(nums, minIndex, i)
+
+
 
 
 def swap(lyst, i, j):
