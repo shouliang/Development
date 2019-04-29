fef10c3d7d8a5f6d4e6a2e8eaf96040a0a299e3f
朱守亮 <git@git.dxl.cc:node/hunqing.git>
2019-04-29 18:09:21 +0800

    add testquick

--- Python/PyDS/sort/logn/test_quick.py
+++ Python/PyDS/sort/logn/test_quick.py
@@ -1,25 +1,31 @@
 def quickSort(nums):
-    if not nums: return []
-    helper(nums,0,len(nums) - 1)
+    if not nums:
+        return []
+    helper(nums, 0, len(nums) - 1)
 
-def helper(nums,low,high):
-    if low>=high: return 
-    pivot = partition(nums,low,high)
-    helper(nums,low,pivot- 1)
-    helper(nums,pivot+1,high)
 
-def partition(nums,low,high):
+def helper(nums, low, high):
+    if low >= high:
+        return
+    pivot = partition(nums, low, high)
+    helper(nums, low, pivot - 1)
+    helper(nums, pivot+1, high)
+
+
+def partition(nums, low, high):
     pivotValue = nums[high]
-    i = low 
-    for j in range(low,high):
+    i = low
+    for j in range(low, high):
         if nums[j] < pivotValue:
-            swap(nums,i,j)
+            swap(nums, i, j)
             i = i + 1
-    swap(nums,i,high)
-    return i 
+    swap(nums, i, high)
+    return i
 
-def swap(nums,i,j):
-    nums[i],nums[j] = nums[j],nums[i]
+
+def swap(nums, i, j):
+    nums[i], nums[j] = nums[j], nums[i]
+
 
 nums = [5, 4, 2, 3, 1, 9, 7, 8, 6]
 quickSort(nums)
