66bddae5696c5434cd421a5deaaefa66afab9a12
shouliang <shouliang0816@163.com>
2019-06-11 22:37:50 +0800

    add struct

---
+++ Go/go_book/src/ch4/embed/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type Point struct {
+	X, Y int
+}
+
+type Circle struct {
+	Point
+	Radius int
+}
+
+type Wheel struct {
+	Circle
+	Spokes int
+}
+
+func main() {
+	w := Wheel{
+		Circle: Circle{
+			Point:  Point{X: 8, Y: 8},
+			Radius: 5,
+		},
+		Spokes: 20,
+	}
+	fmt.Printf("%#v\n", w)
+}
