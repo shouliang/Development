a3cfe99f3abbed427bb47c9f3d1c6280b91d294a
朱守亮 <git@git.dxl.cc:node/hunqing.git>
2019-07-01 17:41:54 +0800

    add goroutine

---
+++ Go/go_book/src/ch8/pipeline2/main.go
@@ -0,0 +1,29 @@
+// Pipeline2 demonstrates a finite 3-stage pipeline.package main
+package main
+
+import "fmt"
+
+func main() {
+	naturals := make(chan int)
+	squares := make(chan int)
+
+	// Counter
+	go func() {
+		for x := 0; x < 100; x++ {
+			naturals <- x
+		}
+		close(naturals)
+	}()
+
+	// Squarer : 使用for range遍历channel
+	go func() {
+		for x := range naturals {
+			squares <- x * x
+		}
+		close(squares)
+	}()
+
+	for x := range squares {
+		fmt.Println(x)
+	}
+}
