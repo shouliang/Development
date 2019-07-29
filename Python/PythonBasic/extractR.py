eef2e3f36ca835324db5478366af134fedb21ac7
朱守亮 <git@git.dxl.cc:node/hunqing.git>
2019-07-29 15:02:39 +0800

    modify newR

--- Python/PythonBasic/extractR.py
+++ Python/PythonBasic/extractR.py
@@ -11,8 +11,9 @@ workbook_new = xlwt.Workbook()
 sheet1_new = workbook_new.add_sheet('newsheet1')
 firstRow = []
 
+csvfilename = 'newR_raw.csv'
 # 提取生成表格
-with open('newR.csv', 'w') as csvfile:
+with open(csvfilename, 'w') as csvfile:
     writer = csv.writer(csvfile)
     # 读取sheet1中的所有行
     for i in range(0, sheet1.nrows):
@@ -24,9 +25,9 @@ with open('newR.csv', 'w') as csvfile:
         result = list(set(result1 + result2))                           # 将提取的内容后去重并转换成list格式
 
         firstRow = list(set(firstRow + result))
-        firstRow = [val for val in firstRow if len(val) > 0]
+        firstRow = [val for val in firstRow if len(val) > 0]          # 过滤去掉空行
         
-        r_content_0 = sheet1.cell(i, 0).value                         # 获取要处理的单元格内容
+        r_content_0 = sheet1.cell(i, 0).value                         # 获取要处理的单元格内容，处理第一列
         pattern = re.compile(r'(^[A-Z]{2,}\d{3,12}-[A-Z]{1}\d{0,})')  # 正则匹配并提取出想要的所有内容
         result0 = pattern.findall(r_content_0)
   
@@ -36,17 +37,23 @@ with open('newR.csv', 'w') as csvfile:
 
 # 将提取的所有列写入第一行： 先读取再重新写入
 allrows = []
-with open('newR.csv', 'r') as csvfile: 
+with open(csvfilename, 'r') as csvfile:    
     csvreader = csv.reader(csvfile) 
     for row in csvreader: 
-        allrows.append(row)      
-
+        newrow=row[0:1]                            #定义新行为第一行
+        for i in range(len(firstRow)):
+            if firstRow[i] in row:
+                newrow.append(1)                   #读取提取后的每行append 为第一行
+            else:
+                newrow.append(0)
+        allrows.append(newrow)      
+        
 firstRow.insert(0,'')
 
-with open('newR.csv', 'w') as csvfile:
+with open(csvfilename, 'w') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(firstRow)
     for line in allrows:
-        writer.writerow(line[0:1])
+        writer.writerow(line)
 
 print(len(firstRow))
