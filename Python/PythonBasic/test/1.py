ef5162e7066c2cea9675b05d02bc9d5c1abd47e8
朱守亮 <git@git.dxl.cc:node/hunqing.git>
2019-08-27 15:40:46 +0800

    add test directory

---
+++ Python/PythonBasic/test/1.py
@@ -0,0 +1,11 @@
+numbers=[12,37,5,42,8,3]
+even=[]
+odd=[]
+while len(numbers)>0:
+    number=numbers.pop()
+    if(number %2==0):
+        even.append(number)
+    else:
+        odd.append(number)
+print(even)
+print(odd)
