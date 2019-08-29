b7e3b02c50d54515e584cb18dff83109224245d0
朱守亮 <git@git.dxl.cc:node/hunqing.git>
2019-08-29 10:50:26 +0800

    add gridsearchcv

---
+++ Python/DataAnalysis/gridsearchcv.py
@@ -0,0 +1,17 @@
+# 使用RandomForest 对IRIS 数据集进行分类
+# 利用GridSearchCV 寻找最优参数
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.datasets import load_iris
+rf = RandomForestClassifier()
+parameters = {"n_estimators": range(1,11)}
+iris = load_iris()
+
+# 使用GridSearchCV进行参数调优
+clf = GridSearchCV(estimator=rf, param_grid=parameters)
+# 对 iris 数据集进行分类
+clf.fit(iris.data, iris.target)
+print(" 最优分数： %.4lf" %clf.best_score_)
+print(" 最优参数： ",clf.best_params_)
+
+
